d6fb4f7a7b08b8629b0b103236a8067d232fffdb
Paweł Jan Czochański <pawel@czochanski.pl>
2023-06-19 06:51:29 +0200

    Extract test helper methods

--- rusqlite_migration/src/tests/asynch.rs
+++ rusqlite_migration/src/tests/asynch.rs
@@ -1,69 +1,13 @@
 use std::iter::FromIterator;
 
-use crate::{AsyncMigrations, Error, MigrationDefinitionError, M};
+use crate::{
+    tests::helpers::{
+        all_valid, m_invalid0, m_invalid1, m_invalid_fk, m_valid0, m_valid10, m_valid11, m_valid20,
+        m_valid21, m_valid_fk,
+    },
+    AsyncMigrations, Error, MigrationDefinitionError,
+};
 use tokio_rusqlite::Connection as AsyncConnection;
-
-fn m_valid0() -> M<'static> {
-    M::up("CREATE TABLE m1(a, b); CREATE TABLE m2(a, b, c);")
-}
-fn m_valid10() -> M<'static> {
-    M::up("CREATE TABLE t1(a, b);")
-}
-fn m_valid11() -> M<'static> {
-    M::up("ALTER TABLE t1 RENAME COLUMN b TO c;")
-}
-fn m_valid20() -> M<'static> {
-    M::up("CREATE TABLE t2(b);")
-}
-fn m_valid21() -> M<'static> {
-    M::up("ALTER TABLE t2 ADD COLUMN a;")
-}
-
-fn m_valid_fk() -> M<'static> {
-    M::up(
-        "CREATE TABLE fk1(a PRIMARY KEY); \
-        CREATE TABLE fk2( \
-            a, \
-            FOREIGN KEY(a) REFERENCES fk1(a) \
-        ); \
-        INSERT INTO fk1 (a) VALUES ('foo'); \
-        INSERT INTO fk2 (a) VALUES ('foo'); \
-    ",
-    )
-    .foreign_key_check()
-}
-
-// All valid Ms in the right order
-fn all_valid() -> Vec<M<'static>> {
-    vec![
-        m_valid0(),
-        m_valid10(),
-        m_valid11(),
-        m_valid20(),
-        m_valid21(),
-        m_valid_fk(),
-    ]
-}
-
-fn m_invalid0() -> M<'static> {
-    M::up("CREATE TABLE table3()")
-}
-fn m_invalid1() -> M<'static> {
-    M::up("something invalid")
-}
-
-fn m_invalid_fk() -> M<'static> {
-    M::up(
-        "CREATE TABLE fk1(a PRIMARY KEY); \
-        CREATE TABLE fk2( \
-            a, \
-            FOREIGN KEY(a) REFERENCES fk1(a) \
-        ); \
-        INSERT INTO fk2 (a) VALUES ('foo'); \
-    ",
-    )
-    .foreign_key_check()
-}
 
 #[tokio::test]
 async fn invalid_migration_statement_test() {
