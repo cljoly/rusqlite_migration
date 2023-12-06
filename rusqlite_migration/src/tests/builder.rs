5d7718a894b0924b680f7868839f2bf0bccd3b59
Clément Joly <7347374+cljoly@users.noreply.github.com>
2023-12-05 22:48:23 -0500

    test: stop installing an old cargo mutants in CI
    Newer versions now build on the codebase. The only issue is that there
    are some more mutants to catch.
    
    Also expand tests to improve coverage

--- rusqlite_migration/src/tests/builder.rs
+++ rusqlite_migration/src/tests/builder.rs
@@ -1,6 +1,8 @@
-use std::iter::FromIterator;
+use std::{iter::FromIterator, num::NonZeroUsize};
 
-use crate::{MigrationsBuilder, M};
+use rusqlite::Connection;
+
+use crate::{Migrations, MigrationsBuilder, SchemaVersion, M};
 
 #[test]
 #[should_panic]
@@ -16,4 +18,28 @@ fn test_0_index() {
     let ms = vec![M::up("CREATE TABLE t(a);")];
 
     let _ = MigrationsBuilder::from_iter(ms).edit(0, move |t| t);
+}
+
+#[test]
+fn test_len_builder() {
+    let mut conn = Connection::open_in_memory().unwrap();
+    // Define migrations
+    let ms = vec![
+        M::up("CREATE TABLE friend(name TEXT);"),
+        M::up("ALTER TABLE friend ADD COLUMN birthday TEXT;"),
+    ];
+
+    {
+        let builder = MigrationsBuilder::from_iter(ms);
+
+        let migrations: Migrations = builder.finalize();
+
+        migrations.to_latest(&mut conn).unwrap();
+
+        assert_eq!(migrations.ms.len(), 2);
+        assert_eq!(
+            Ok(SchemaVersion::Inside(NonZeroUsize::new(2).unwrap())),
+            migrations.current_version(&conn)
+        );
+    }
 }
