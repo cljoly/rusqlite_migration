4d0a408c4556bd4ee1124c0846d20fd3bb2b8ba3
Shawn L. <shawn@sola.sh>
2025-02-01 17:10:40 -0500

    chore!: remove async support
    Remove the special async feature as discussed in https://github.com/cljoly/rusqlite_migration/issues/226.
    
    Closes #226

--- examples/async/src/main.rs
+++ examples/async/src/main.rs
@@ -2,23 +2,31 @@ use std::sync::LazyLock;
 
 use anyhow::Result;
 use rusqlite::params;
-use rusqlite_migration::{AsyncMigrations, M};
+use rusqlite_migration::{Migrations, M};
 use tokio_rusqlite::Connection;
+
+/// The general idea with this example is to use [`Connection::call`][call] and
+/// [`Connection::call_unwrap`][call_unwrap] to run the migration in a sync context.
+///
+/// [call]: https://docs.rs/tokio-rusqlite/0.6.0/tokio_rusqlite/struct.Connection.html#method.call
+/// [call_unwrap]: https://docs.rs/tokio-rusqlite/0.6.0/tokio_rusqlite/struct.Connection.html#method.call_unwrap
 
 // Test that migrations are working
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[tokio::test]
-    async fn migrations_test() {
-        assert!(MIGRATIONS.validate().await.is_ok());
+    // Validating that migrations are correctly defined. It is enough to test in the sync context,
+    // because under the hood, tokio_rusqlite executes the migrations in a sync context anyway.
+    #[test]
+    fn migrations_test() {
+        assert!(MIGRATIONS.validate().is_ok());
     }
 }
 
 // Define migrations. These are applied atomically.
-static MIGRATIONS: LazyLock<AsyncMigrations> = LazyLock::new(|| {
-    AsyncMigrations::new(vec![
+static MIGRATIONS: LazyLock<Migrations> = LazyLock::new(|| {
+    Migrations::new(vec![
         M::up(include_str!("../../friend_car.sql")),
         // PRAGMA are better applied outside of migrations, see below for details.
         M::up(
@@ -28,27 +36,36 @@ static MIGRATIONS: LazyLock<AsyncMigrations> = LazyLock::new(|| {
                   "#,
         ),
         // This migration can be reverted
-        M::up("CREATE TABLE animal(name TEXT);").down("DROP TABLE animal;"), // In the future, if the need to change the schema arises, put
-                                                                             // migrations here, like so:
-                                                                             // M::up("CREATE INDEX UX_friend_email ON friend(email);"),
-                                                                             // M::up("CREATE INDEX UX_friend_name ON friend(name);"),
+        M::up("CREATE TABLE animal(name TEXT);").down("DROP TABLE animal;"),
+        // In the future, if the need to change the schema arises, put
+        // migrations here, like so:
+        // M::up("CREATE INDEX UX_friend_email ON friend(email);"),
+        // M::up("CREATE INDEX UX_friend_name ON friend(name);"),
     ])
 });
 
 pub async fn init_db() -> Result<Connection> {
-    let mut async_conn = Connection::open("./my_db.db3").await?;
+    let async_conn = Connection::open("./my_db.db3").await?;
 
     // Update the database schema, atomically
-    MIGRATIONS.to_latest(&mut async_conn).await?;
+    // Using `call_unwrap` is appropriate because we have just opened the connection before and
+    // this function still returns any error returned inside of it. In other words, according to
+    // the [documentation][docs], it only panics if there is an issue with the connection, not if
+    // an error is returned by the closure.
+    //
+    // [docs]: https://docs.rs/tokio-rusqlite/0.6.0/tokio_rusqlite/struct.Connection.html#method.call_unwrap
+    async_conn
+        .call_unwrap(|conn| MIGRATIONS.to_latest(conn))
+        .await?;
 
     Ok(async_conn)
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("trace")).init();
 
-    let mut async_conn = init_db().await.unwrap();
+    let async_conn = init_db().await.unwrap();
 
     // Apply some PRAGMA. These are often better applied outside of migrations, as some needs to be
     // executed for each connection (like `foreign_keys`) or to be executed outside transactions
@@ -82,12 +99,19 @@ async fn main() {
         .unwrap()
         .unwrap();
 
-    // If we want to revert the last migration
-    MIGRATIONS.to_version(&mut async_conn, 2).await.unwrap();
+    // We can revert to the last migration
+    // Let’s also demonstrate using `Connection::call` instead of `Connection::call_unwrap`.
+    // Notice how in effect, we have handle two `Result`s, one for the errors happening when we try
+    // to use the connection and the other one when applying the migration proper.
+    async_conn
+        .call(|conn| Ok(MIGRATIONS.to_version(conn, 2)))
+        .await??;
 
     // The table was removed
     async_conn
         .call(|conn| Ok(conn.execute("INSERT INTO animal (name) VALUES (?1)", params!["cat"])))
         .await
         .unwrap_err();
+
+    Ok(())
 }
